0be50036dd523610b459c2da2adf1910cb35f5e4
slyRacoon23 <earl.potters@gmail.com>
2022-03-19 19:46:19 -0600

    change account

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -29,7 +29,7 @@ const config: HardhatUserConfig = {
   networks: { 
     localhost: {
     chainId: 31337, // Chain ID should match the hardhat network's chainid
-    accounts: [`0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e`],
+    accounts: [`0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`],
   },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
